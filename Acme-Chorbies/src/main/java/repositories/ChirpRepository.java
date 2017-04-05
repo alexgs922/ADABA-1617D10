2d62dff0ec32e5b46594a1457faaa47f6ab8a7af
adrluqmon <adrluqmon@alum.us.es>
2017-04-06 00:12:58 +0200

    add all user cases about chirps

--- Acme-Chorbies/src/main/java/repositories/ChirpRepository.java
+++ Acme-Chorbies/src/main/java/repositories/ChirpRepository.java
@@ -1,12 +1,21 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Chirp;
 
 @Repository
 public interface ChirpRepository extends JpaRepository<Chirp, Integer> {
+
+	@Query("select m from Chorbi c join c.chirpReceives m where m.copy=false and c.id=?1")
+	Collection<Chirp> myRecivedMessages(int actorId);
+
+	@Query("select m from Chorbi c join c.chirpWrites m where m.copy=true and c.id=?1")
+	Collection<Chirp> mySendedMessages(int actorId);
 
 }
